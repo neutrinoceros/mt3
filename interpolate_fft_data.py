fad1e415e9e56a6a57518f841916d63aa838f995
clementhottier <hottierclement@gmail.com>
2016-01-22 17:38:58 +0100

    little modification i have to finish the fft of the vlbi data

--- interpolate_fft_data.py
+++ interpolate_fft_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sc
+import scipy.fftpack as fftp
 import matplotlib.pyplot as plt
 
 data =np.genfromtxt("data/opa2015a.eops",usecols=(0,4,5,9,10))
@@ -15,12 +16,15 @@ data[:,0]=data[:,0]-data[0,0]
 sample_step=3 #days
 
 #final time to interpolate the time
-final_time=(int(data[-1,0]/sample_step)+2)*sample_step
-
+final_time=(int(data[-1,0]/sample_step)+1)*sample_step
+print "derniertemps",data[-1,0]
+print "final time",final_time
 
 #interpolation of data
-interpolated_time=np.arange(0,final_time,float(sample_step))
-
+interpolated_time=np.arange(0,final_time+sample_step,float(sample_step))
+print "dernier temps interpoller",interpolated_time[-1] 
+print "longeur tab intrerpoller",len(interpolated_time)
+print "temps final * sample",final_time*sample_step
 interpolated_dX=np.interp(interpolated_time,data[:,0],data[:,1])
 interpolated_dY=np.interp(interpolated_time,data[:,0],data[:,2])
 
@@ -28,4 +32,4 @@ interpolated_dY=np.interp(interpolated_time,data[:,0],data[:,2])
 #checking plot
 plt.plot(data[:,0],data[:,1],'b')
 plt.plot(interpolated_time,interpolated_dX,'r x')
-plt.show()
+# plt.show()
