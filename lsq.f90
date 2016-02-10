95062142815851fc418e348dc509528c840d7b58
clementhottier <hottierclement@gmail.com>
2016-02-10 16:51:58 +0100

    add some comment and fix FIXME

--- lsq.f90
+++ lsq.f90
@@ -19,23 +19,23 @@ program lsq
   integer,parameter::Nbr_of_point=5980,Nbr_of_parameter=42
   integer, dimension(Nbr_of_parameter) :: l,ls,F,D,Om !integer multiplicativ coefficent to process modeling pulsation
   real (kind=xi), dimension(Nbr_of_parameter) :: ReREN,ImREN,ReMHB,ImMHB, &
-    sigma,phi
+    sigma,phi !reading variable not use in the code
   real(kind=xi) :: Me,Ve,Te,Ma,Ju,Sa,Ur,Ne,Pa, lpar1,lpar2,lpar3,lspar1, &
     lspar2, lspar3,Fpar1,Fpar2,Fpar3,Dpar1,Dpar2,Dpar3,omegapar1, omegapar2, &
-    omegapar3, ReCOR,ImCOR,ReADD,ImADD,ReRET,ImRET,Periode
-  character(10) :: Aj
+    omegapar3, ReCOR,ImCOR,ReADD,ImADD,ReRET,ImRET,Periode !reading variable not use in the code
+  character(10) :: Aj !reading variable not use in the code
 
-  real(kind=xi), dimension(Nbr_of_point) :: t, dX, dY, errdX, errdY, corrdXdY
-  real(kind=xi), dimension(2*Nbr_of_point) :: dXdY
-  real(kind=xi) :: var
-  character(20) :: carc
+  real(kind=xi), dimension(Nbr_of_point) :: t, dX, dY, errdX, errdY, corrdXdY !parameter of observed nutation
+  real(kind=xi), dimension(2*Nbr_of_point) :: dXdY !matrix of observable
+  real(kind=xi) :: var !reading variable
+  character(20) :: carc !reading variable
 
-  real(kind=xi), dimension(2*Nbr_of_point,2*Nbr_of_parameter) :: M
-  real(kind=xi), dimension(2*Nbr_of_parameter,2*Nbr_of_parameter) :: MM, P
+  real(kind=xi), dimension(2*Nbr_of_point,2*Nbr_of_parameter) :: M !matrix like dXdY = M Ampl
+  real(kind=xi), dimension(2*Nbr_of_parameter,2*Nbr_of_parameter) :: MM, P !MM is equal to tranpose(M) matrix product M
   real(kind=xi), dimension(2*Nbr_of_parameter,2*Nbr_of_point) :: Q, MMM
-  real(kind=xi), dimension(2*Nbr_of_parameter) :: Ampl
+  real(kind=xi), dimension(2*Nbr_of_parameter) :: Ampl !complex amplitude that we need to adjust
   real(kind=xi), dimension(Nbr_of_parameter) :: A, B
-  integer :: i, j, k, r, s
+  integer :: i, j, k, r, s !loop variable
 
 !Read "ondes.txt" data  
   open (unit=15, file="data/ondes.txt", status='old')
@@ -62,18 +62,23 @@ program lsq
          carc, errdX(j), errdY(j), carc, var, var, var, corrdXdY(j), &
          var, carc, var, var, var, var, var, var, var, var, &
          var, var, var, var, carc
+    
+    !some data have 0.000 as error that meen tha the value is not true
+    !we change this error in 100000. to kill this point
     if (errdX(j) .lt. 0.0009) then
-       errdX(j) = 0.0001 !FIXME why 0.00001 ? and not 10000 ???
+       errdX(j) = 100000.
     end if
     if (errdY(j) .lt. 0.0009) then
-       errdY(j) = 0.0001 !FIXME same thing 
+       errdY(j) = 100000.
     end if
+
     ! s = Nbr_of_point + j
     ! dXdY(j) = dX(j)
     ! dXdY(s) = dY(j)
   end do
   close(10)
 
+  !computation of the observationnal array
   dXdY(:Nbr_of_point)=dX/errdX
   dXdY(Nbr_of_point+1:)=dY/errdY
 
