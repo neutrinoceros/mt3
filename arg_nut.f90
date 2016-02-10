8672ead238725033819af6faee4b25fcb082ff23
clementhottier <hottierclement@gmail.com>
2016-02-10 17:57:01 +0100

    add a FIXME Lambert said us to check if the unity of amplitude and phase is in 2*pi/days but i dont know how doing this

--- arg_nut.f90
+++ arg_nut.f90
@@ -10,22 +10,23 @@ module arg_nut
 ! Variables declaration
 !======================================================================  
   
-  integer, parameter:: xi = selected_real_kind (15)
-  real (kind=xi), parameter :: pi=4._xi*atan(1._xi)
+  integer, parameter        :: xi = selected_real_kind (15)
+  real (kind=xi), parameter :: pi = 4._xi*atan(1._xi)
 
   !phi.. (phase) equal to constant part of the equation
-  real(kind=xi), parameter :: phil = 134.96340251_xi*pi/180._xi 
-  real(kind=xi), parameter :: phils = (357.52910918_xi*pi/180._xi)
-  real(kind=xi), parameter :: phiF = 93.27209062_xi*pi/180._xi
-  real(kind=xi), parameter :: phiD = (297.85019547_xi*pi/180._xi)
-  real(kind=xi), parameter :: phiomega = 125.04455501_xi*pi/180._xi
+  !FIXME we need to check the unity of this parameter like Lambert said us, but now i don't know how doing that
+  real(kind = xi), parameter :: phil     = 134.96340251_xi*pi/180._xi
+  real(kind = xi), parameter :: phils    = 357.52910918_xi*pi/180._xi
+  real(kind = xi), parameter :: phiF     = 93.27209062_xi*pi/180._xi
+  real(kind = xi), parameter :: phiD     = 297.85019547_xi*pi/180._xi
+  real(kind = xi), parameter :: phiomega = 125.04455501_xi*pi/180._xi
 
   !sigma.. (frequency) equal to coeficient of "t" of the equation
-  real(kind=xi), parameter :: sigmal =(1717915923.2178_xi/3600._xi)*pi/180._xi
-  real(kind=xi), parameter :: sigmals =(129596581.0481_xi/3600._xi)*pi/180._xi
-  real(kind=xi), parameter :: sigmaF =(1739527262.8478_xi/3600._xi)*pi/180._xi
-  real(kind=xi), parameter :: sigmaD =(1602961601.2090_xi/3600._xi)*pi/180._xi
-  real(kind=xi), parameter :: sigmaomega =(-6962890.5431_xi/3600._xi)*pi/180._xi
+  real(kind = xi), parameter :: sigmal     = 1717915923.2178_xi/3600._xi*pi/180._xi
+  real(kind = xi), parameter :: sigmals    = 129596581.0481_xi/3600._xi*pi/180._xi
+  real(kind = xi), parameter :: sigmaF     = 1739527262.8478_xi/3600._xi*pi/180._xi
+  real(kind = xi), parameter :: sigmaD     = 1602961601.2090_xi/3600._xi*pi/180._xi
+  real(kind = xi), parameter :: sigmaomega = -6962890.5431_xi/3600._xi*pi/180._xi
 
   private 
 
