8782484733d5f0662e574bfb69c6a51783ff2571
clementhottier <hottierclement@gmail.com>
2016-02-12 14:47:47 +0100

    add a module to process the amplitude to a given f

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ all : lsq lsq_457
 lsq : mod_matrix.o arg_nut.o lsq.f90 
 	$(FC) $(FLAGS) $^ -o lsq.exe $(LFLAG)
 
-lsq_457 : mod_matrix.o arg_nut.o lsq_457_day_term.f90
+lsq_457 : mod_matrix.o mod_lsq.o arg_nut.o lsq_457_day_term.f90
 	$(FC) $(FLAGS) $^ -o lsq_457.exe $(LFLAG)
 
 arg_nut.o : arg_nut.f90
@@ -16,6 +16,9 @@ arg_nut.o : arg_nut.f90
 
 mod_matrix.o : mod_matrix.f90
 	$(FC) -c $^ 
+
+mod_lsq.o : mod_lsq.f90
+	$(FC) -c $^
 
 run: lsq
 	./lsq.exe
---
+++ mod_lsq.f90
@@ -0,0 +1,73 @@
+module mod_lsq
+  use mod_matrix
+  use arg_nut
+  implicit none
+contains
+
+  !module to compute 
+  subroutine processing_lsq_period (periode,Nbr_of_parameter,Nbr_of_point,&
+      measure_array_X,measure_array_Y,error_array_X,error_array_Y,time,&
+      amplitude_array)
+    !Subroutine to processe the amplitude of 457 days nutation periode
+    implicit none
+
+    !========================!
+    !argument of the function!
+    !------------------------!
+    real(kind=xi),                         intent (in) :: periode ! periode that we want to fit the parameter
+    integer,                               intent (in) :: Nbr_of_parameter ! nbr of parameter we want to fit
+    integer,                               intent (in) :: Nbr_of_point ! nbr of measure
+    real(kind=xi),dimension(Nbr_of_point), intent (in) :: measure_array_X, measure_array_Y ! array containing the measure point
+    real(kind=xi),dimension(Nbr_of_point), intent (in) :: error_array_X,error_array_Y !array contains the error of measure
+    real(kind=xi),dimension(Nbr_of_point), intent (in) :: time !time of th measure array
+
+    real(kind=xi),dimension(2*Nbr_of_parameter), intent(out) :: amplitude_array ! amplitude we want to process 
+    !========================!
+
+    !==============!
+    !local variable!
+    !--------------!
+
+    real(kind=xi) :: sigma 
+
+    real(kind=xi), dimension(2*Nbr_of_point) :: dXdY !matrix of observable
+
+    real(kind=xi), dimension(2*Nbr_of_point,2*Nbr_of_parameter)     :: M !matrix like dXdY = M Ampl
+    real(kind=xi), dimension(2*Nbr_of_parameter,2*Nbr_of_parameter) :: MM, P !MM is equal to tranpose(M) matrix product M
+    real(kind=xi), dimension(2*Nbr_of_parameter,2*Nbr_of_point)     :: Q, MMM
+    real(kind=xi), dimension(2*Nbr_of_parameter)                    :: Ampl !complex amplitude that we need to adjust
+
+    integer :: i, j, k, r, s !loop variable
+
+    !==============!
+    sigma = 2._xi*pi/periode
+
+    !Creation of the matrix M
+    do j=1,Nbr_of_point
+      s = Nbr_of_point + j
+
+      M(j,1) = 1./error_array_X(j)*cos(sigma*time(j))
+      M(j,2) = 1./error_array_X(j)*sin(sigma*time(j))
+
+      M(s,1) = -1./error_array_Y(j)*sin(sigma*time(j))
+      M(s,2) = 1./error_array_Y(j)*cos(sigma*time(j))
+
+    end do
+
+    !computation of the observationnal array
+    dXdY(:Nbr_of_point)   = measure_array_X/error_array_x
+    dXdY(Nbr_of_point+1:) = measure_array_Y/error_array_Y
+
+    !Inverse matrix with A = ((M[t]*M)^-1)*(M[t]*X)
+    Q = transpose(M) ! Transopse the Matrix
+    MM = matmul(Q,M) ! Multiply the Matrix
+    P = inv(MM)      ! Inverse the Matrix using function which defined
+    MMM = matmul(P,Q)
+
+    amplitude_array = matmul(MMM,dXdY)
+
+
+  end subroutine processing_lsq_period
+
+end module mod_lsq
+
