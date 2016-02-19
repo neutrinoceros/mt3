37964d3627a21c4f6c6dcb23e2753aea23029da2
clementhottier <hottierclement@gmail.com>
2016-02-19 16:10:41 +0100

    make interpoaltion of 457 days work sorry for your heart

--- interpolate_457_days_term.py
+++ interpolate_457_days_term.py
@@ -12,15 +12,8 @@
 #====================================================================================================
 
 import numpy as np
-import pylab as pl
-
-
-#data loading
-#====================================================================================================
-tab   = np.loadtxt('457_days_ampl.dat')
-time,xamp,yamp = tab[:,0],tab[:,1],tab[:,2]
-
-time_d = np.loadtxt('data/opa2015a.eops',usecols=[0])
+# import pylab as pl
+# import matplotlib.pyplot as plt
 
 
 #interpolation function
@@ -48,23 +41,52 @@ def interp_func(t,dim) :
 
 #application and data saving
 #====================================================================================================
-xamp_inter = np.array([interp_func(t,dim=1) for t in time_d])
-yamp_inter = np.array([interp_func(t,dim=2) for t in time_d])
+# xamp_inter = np.array([interp_func(t,dim=1) for t in time_d])
+# yamp_inter = np.array([interp_func(t,dim=2) for t in time_d])
 
-tab_final = np.column_stack([time_d,xamp_inter,yamp_inter])
+# tab_final = np.column_stack([time_d,xamp_inter,yamp_inter])
+# np.savetxt('interpolated_457d_amps.dat',tab_final,delimiter='    ')
+
+#data loading
+#====================================================================================================
+tab   = np.loadtxt('457_days_ampl.dat')
+time,xamp,yamp = tab[:,0],tab[:,1],tab[:,2]
+
+time_d = np.loadtxt('data/opa2015a.eops',usecols=[0])
+
+
+
+#trying with the interne function 
+#================================
+x_interp=np.interp(time_d,time,xamp)
+y_interp=np.interp(time_d,time,yamp)
+
+#saving value
+#============
+tab_final = np.column_stack([x_interp,y_interp])
 np.savetxt('interpolated_457d_amps.dat',tab_final,delimiter='    ')
+
+
+
+
+# plt.plot(time,xamp,'r-',label='data')
+# plt.scatter(time_d,x_interp,label='interp',alpha=0.1)
+# plt.legend(loc='best')
+# plt.show()
+
+
 
 
 #for test/visualization only
 #====================================================================================================
-#time_i = np.arange(min(time),max(time),1e-1)
-#xamp_i = np.array([interp_func(t) for t in time_i])
+# time_i = np.arange(min(time),max(time),1e-1)
+# xamp_i = np.array([interp_func(t) for t in time_i])
 
 # fig,ax = pl.subplots()
 # pl.ion()
 # pl.show()
 # ax.scatter(time,xamp)
-# #ax.scatter(time_d,time_d)
+# # ax.scatter(time_d,time_d)
 # ax.plot(time_i,xamp_i)
 # pl.draw()
 # pl.ioff()
