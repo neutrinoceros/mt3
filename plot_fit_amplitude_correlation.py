9ad5429ab5d230b6ae99c5b3a5574f5e53cea32e
clementhottier <hottierclement@gmail.com>
2016-02-26 14:24:49 +0100

    add a ploting script in order to plot the correlation matrix

---
+++ plot_fit_amplitude_correlation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+correlation=np.loadtxt('fit_amplitude_correlation.dat')
+
+
+plt.matshow(correlation,cmap=plt.cm.viridis)
+axenumx=np.array([21,41.5,62,84,87])
+axenumy=np.array([21,41.5,62,84,87])
+axelegendx=('$A_{re}$','|','$A_{im}$','|','$cst$')
+axelegendy=('$A_{re}$','-','$A_{im}$','-','$cst$')
+plt.xticks(axenumx,axelegendx)
+plt.yticks(axenumy,axelegendy)
+plt.colorbar()
+
+plt.savefig('pictures/fit_amplitude_correlation.pdf')
+# plt.show()
